76b97227b9058b0cfbd006f0460b7c03597aa932
JayG1212 <133731996+JayG1212@users.noreply.github.com>
2025-11-11 19:58:34 -0600

    Pivot1
    Uses a Pivot to display the number of vehicles by type per branch

---
+++ Pivot1.sql
@@ -0,0 +1,25 @@
+-- Pivots
+
+-- Uses a Pivot to display the number of vehicles by type per branch
+-- Business Purpose: Summarizes the number of vehicles by type at each branch.
+-- Helps management analyze the vehicle distribution across locations and support inventory allocation decisions
+
+
+SELECT branch_name as BranchName, [Crossover], [Sedan], [SUV], [Truck], [Van]
+FROM
+(
+	SELECT B.branch_name, VT.vehicle_Type_Name, V.vehicle_type_id
+	FROM Vehicle.Vehicle as V
+	INNER JOIN Vehicle.Vehicle_Type as VT
+	ON V.vehicle_type_id = VT.vehicle_type_id
+	INNER JOIN Operation.Branch as B
+	ON B.branch_id = V.branch_id
+) as PivotData
+
+PIVOT
+(
+	COUNT(vehicle_type_ID)
+	FOR vehicle_type_name in ([Crossover], [Sedan], [SUV], [Truck], [Van])
+) AS PivotTable
+ORDER BY BranchName
+
