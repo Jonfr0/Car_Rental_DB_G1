7d40f010917d0df59f87e65832bbbd4d4c3c5d2c
Fardowsai <156835599+Fardowsai@users.noreply.github.com>
2025-11-15 14:33:53 -0600

    Create Cursor.sql

---
+++ Cursor.sql
@@ -0,0 +1,27 @@
+DECLARE @audit VARCHAR(500);
+
+DECLARE rental_cursor CURSOR FOR
+SELECT 
+    'CustomerID: ' + CAST(customer_id AS VARCHAR(20)) +
+    ' RentalID: ' + CAST(rental_id AS VARCHAR(20)) +
+    ' Start: ' + CAST(rental_start AS VARCHAR(30)) +
+    ' End: ' + CAST(rental_end AS VARCHAR(30)) +
+    ' Status: ' + rental_status +
+    ' Total: ' + CAST(rental_total AS VARCHAR(20))
+FROM Rental.Rental
+ORDER BY customer_id, rental_id;
+
+OPEN rental_cursor;
+
+FETCH NEXT FROM rental_cursor INTO @audit;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT @audit;
+    FETCH NEXT FROM rental_cursor INTO @audit;
+END
+
+CLOSE rental_cursor;
+DEALLOCATE rental_cursor; 
+DEALLOCATE rental_cursor;
+
